856f704fe4edf47fef4af2d27ca32fa7b6377e28
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-11-20 13:45:54 +0000

    Feat: added container component to main sections

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,8 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 import { Button } from "./ui/button";
+import { useRegisterModal } from "@/hooks/useModal";
+import Container from "./container";
 
 const titan = Titan_One({
   subsets: ["latin"],
@@ -14,36 +16,44 @@ const titan = Titan_One({
 });
 
 const Header = () => {
+  const { onOpen } = useRegisterModal();
   const router = useRouter();
 
   return (
-    <header className="flex justify-between items-center py-2">
-      <Link
-        href={"/"}
-        className={`${titan.className} flex items-center gap-x-2`}
-      >
-        <Image src={"/logo.svg"} alt="Logo" width={60} height={60} />
-        <span className="text-2xl text-primary-main">Pizza</span>
-      </Link>
-      <nav className="flex items-center gap-x-2 font-bold z-10">
-        {/* TODO: Adicionar Modal para login e registrar-se */}
-        <Button variant="ghost" className='text-white hover:text-white hover:bg-white/10' onClick={() => router.push("/login")}>
-          Iniciar sessão
-        </Button>
-        <Button
-          variant="main"
-          onClick={() => router.push("/register")}
-          className="flex items-center gap-x-2 py-2 px-4 transition"
-        >
-          <User2 className="h-6 w-6" />
-          Registrar-se
-        </Button>
-
-        <Button variant="ghost" className='hover:bg-primary-main/20'>
-          {/* TODO: Add shoppingCart functionality and component */}
-          <ShoppingCart className="h-6 w-6  text-primary-main" />
-        </Button>
-      </nav>
+    <header>
+      <Container>
+        <div className="flex justify-between items-center py-2">
+          <Link
+            href={"/"}
+            className={`${titan.className} flex items-center gap-x-2`}
+          >
+            <Image src={"/logo.svg"} alt="Logo" width={60} height={60} />
+            <span className="text-2xl text-primary-main">Pizza</span>
+          </Link>
+          <nav className="flex items-center gap-x-2 font-bold z-10">
+            <Button variant="ghost" className="hover:bg-primary-main/20">
+              {/* TODO: Add shoppingCart functionality and component */}
+              <ShoppingCart className="h-6 w-6  text-primary-main" />
+            </Button>
+            {/* TODO: Adicionar Modal para login e registrar-se */}
+            <Button
+              variant="ghost"
+              className="text-white hover:text-white hover:bg-white/10"
+              onClick={() => router.push("/login")}
+            >
+              Iniciar sessão
+            </Button>
+            <Button
+              variant="main"
+              onClick={onOpen}
+              className="flex items-center gap-x-2 py-2 px-4 transition"
+            >
+              <User2 className="h-6 w-6" />
+              Registrar-se
+            </Button>
+          </nav>
+        </div>
+      </Container>
     </header>
   );
 };
