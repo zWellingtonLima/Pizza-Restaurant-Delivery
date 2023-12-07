c9daf12266c560bd784bd5dbc1db053a4b259710
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-12-07 17:49:48 +0000

    refac: added className prop to container

--- components/container.tsx
+++ components/container.tsx
@@ -1,9 +1,16 @@
+import { cn } from "@/lib/utils";
+
 interface ContainerProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const Container = ({ children }: ContainerProps) => {
-  return <div className="mx-auto w-full px-4 max-w-7xl">{children}</div>;
+const Container = ({ children, className }: ContainerProps) => {
+  return (
+    <div className={cn("mx-auto w-full px-4 max-w-7xl", className)}>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
